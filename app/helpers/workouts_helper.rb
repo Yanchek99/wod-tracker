beff08a2aa323d5159f74d15b3e08456179b2cb0
Matt Yanchek <Yanchek99@users.noreply.github.com>
2018-04-20 15:13:38 -0700

    Bug in workout helper

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -3,7 +3,7 @@ module WorkoutsHelper
     return "#{pluralize workout.rounds, 'round'} for time" if workout.rounds_for_time?
     return "As many rounds as possible in #{pluralize workout.time, 'minute'}" if workout.amrap?
     return "EMOM #{workout.time}" if workout.emom?
-    return "#{workout.rounds} #{time}-minute rounds" if workout.timed_rounds?
+    return "#{workout.rounds} #{workout.time}-minute rounds" if workout.timed_rounds?
     workout.interval
   end
 end

a23aa9f3e3679c5034017199acd04b506731db01
Matt Yanchek <Yanchek99@users.noreply.github.com>
2018-05-04 15:26:54 -0700

    UI Tweaks

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,3 +1,17 @@
 class Movement < ApplicationRecord
   enum measurement: [:weight, :distance, :time, :height]
+
+  def measurement_unit
+    return '' unless measurement
+    case measurement.to_sym
+    when :weight
+      'lb'
+    when :distance
+      'meter'
+    when :time
+      'minute'
+    else
+      ''
+    end
+  end
 end

c52c58cfcf512412de82cef97a5129b48f88da94
Matt Yanchek <Yanchek99@users.noreply.github.com>
2018-10-20 20:00:31 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_222630) do
+ActiveRecord::Schema.define(version: 2018_07_17_143049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 2018_07_06_222630) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "metrics", force: :cascade do |t|
+    t.bigint "exercise_id"
+    t.string "measurement"
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["exercise_id"], name: "index_metrics_on_exercise_id"
   end
 
   create_table "movement_logs", force: :cascade do |t|

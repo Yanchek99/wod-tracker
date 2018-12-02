2092b17692e59fec2cce4ff2ab19b32b4def0933
Matt Yanchek <Yanchek99@users.noreply.github.com>
2018-12-02 09:54:04 -0800

    Add schedule views (#46)
    * Add Schedules
    
    * Fix admin and schedule bug

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,5 +2,5 @@ class Schedule < ApplicationRecord
   belongs_to :program
   belongs_to :workout
 
-  scope :posted_dates, -> { order(posted_at: :desc).select(:posted_at).distint }
+  scope :posted_dates, -> { order(posted_at: :desc).select(:posted_at).distinct }
 end

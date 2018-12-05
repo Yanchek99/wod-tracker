9357010f9afdc68d7aa000a79b0ef80f1b495876
Matt Yanchek <Yanchek99@users.noreply.github.com>
2018-12-05 05:27:51 -0800

    commit rails upgrade and rubocop fix

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,6 +4,7 @@ class SchedulesController < ApplicationController
   def index
     @schedules = Current.user.schedules
     return if @schedules.empty?
+
     @dates = @schedules.posted_dates.page(params[:page]).per(1)
     @date = @dates.first.posted_at.to_date
     @schedules = @schedules.where(posted_at: @date.beginning_of_day...@date.end_of_day)

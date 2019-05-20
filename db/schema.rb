5de6b8730e09b5905076f91336ea1bcdc429579e
Matt Yanchek <Yanchek99@users.noreply.github.com>
2019-05-19 17:20:33 -0700

    Add time cap to workout (#76)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_022236) do
+ActiveRecord::Schema.define(version: 2019_05_19_210810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(version: 2019_02_12_022236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "notes"
+    t.integer "time_cap_seconds"
   end
 
   add_foreign_key "exercises", "movements"

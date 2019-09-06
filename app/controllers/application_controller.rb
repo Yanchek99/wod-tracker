489ff14264a1367506ada1273a6bc50636254a58
Matt Yanchek <Yanchek99@users.noreply.github.com>
2019-09-06 09:47:38 -0700

    Add weight to reg form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name weight])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name weight])
   end
 end

9c2e41df36903512b722aaacfafa7626d430f75e
Matt Yanchek <Yanchek99@users.noreply.github.com>
2020-07-12 08:31:17 -0700

    update rubop

--- app/inputs/group_input.rb
+++ app/inputs/group_input.rb
@@ -7,12 +7,12 @@ class GroupInput < SimpleForm::Inputs::Base
   end
 
   def prepend(_wrapper_options)
-    span_tag = content_tag(:span, options[:prepend], class: 'input-group-text')
-    template.content_tag(:div, span_tag, class: 'input-group-prepend')
+    span_tag = tag.span options[:prepend], class: 'input-group-text'
+    template.tag.div span_tag, class: 'input-group-prepend'
   end
 
   def append(_wrapper_options)
-    span_tag = content_tag(:span, options[:append], class: 'input-group-text')
-    template.content_tag(:div, span_tag, class: 'input-group-append')
+    span_tag = tag.span options[:append], class: 'input-group-text'
+    template.tag.div span_tag, class: 'input-group-append'
   end
 end

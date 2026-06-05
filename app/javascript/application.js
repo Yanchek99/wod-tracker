add7981668213c81855f61cac8c6a53162c6a2b8
Matt Yanchek <Yanchek99@users.noreply.github.com>
2026-06-05 13:37:51 -0700

    Reduce production JavaScript size

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,6 +6,14 @@ import "./controllers"
 
 import * as bootstrap from "bootstrap"
 import LocalTime from "local-time"
-import "@fortawesome/fontawesome-free/js/all"
+import { dom, library } from "@fortawesome/fontawesome-svg-core"
+import { faEdit } from "@fortawesome/free-solid-svg-icons/faEdit"
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons/faExternalLinkAlt"
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus"
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons/faTrashAlt"
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser"
+import { faCheckSquare } from "@fortawesome/free-regular-svg-icons/faCheckSquare"
 
 LocalTime.start()
+library.add(faCheckSquare, faEdit, faExternalLinkAlt, faPlus, faTrashAlt, faUser)
+dom.watch()

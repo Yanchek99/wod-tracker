ee467365d9e97329fbafe922c3437ca74b051664
Matt Yanchek <Yanchek99@users.noreply.github.com>
2026-06-16 11:58:30 -0700

    Avoid persistent grey drag state

--- app/javascript/controllers/sortable_list_controller.js
+++ app/javascript/controllers/sortable_list_controller.js
@@ -12,10 +12,11 @@ export default class extends Controller {
       ghostClass: "workout-sortable-ghost",
       chosenClass: "workout-sortable-chosen",
       dragClass: "workout-sortable-drag",
-      onEnd: () => {
+      onEnd: (event) => {
         this.refresh()
-        this.clearDragClasses()
-      }
+        this.clearDragState(event.item)
+      },
+      onUnchoose: (event) => this.clearDragState(event.item)
     })
 
     this.refresh()
@@ -32,12 +33,24 @@ export default class extends Controller {
     })
   }
 
-  clearDragClasses() {
-    requestAnimationFrame(() => {
-      this.containerTarget.querySelectorAll(".sortable-ghost, .sortable-chosen, .sortable-drag, .workout-sortable-ghost, .workout-sortable-chosen, .workout-sortable-drag").forEach((item) => {
-        item.classList.remove("sortable-ghost", "sortable-chosen", "sortable-drag", "workout-sortable-ghost", "workout-sortable-chosen", "workout-sortable-drag")
-      })
+  clearDragState(item) {
+    [0, 50, 200].forEach((delay) => {
+      setTimeout(() => this.clearDragClasses(item), delay)
     })
+  }
+
+  clearDragClasses(item) {
+    this.dragClassElements(item).forEach((element) => {
+      element.classList.remove("sortable-ghost", "sortable-chosen", "sortable-drag", "workout-sortable-ghost", "workout-sortable-chosen", "workout-sortable-drag")
+    })
+  }
+
+  dragClassElements(item) {
+    const selector = ".sortable-ghost, .sortable-chosen, .sortable-drag, .workout-sortable-ghost, .workout-sortable-chosen, .workout-sortable-drag"
+    const elements = Array.from(document.querySelectorAll(selector))
+    if (item) elements.push(item)
+
+    return elements
   }
 
   get items() {

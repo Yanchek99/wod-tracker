c0fea8087deb16dee9291eb0ad1f8e7990118a66
Matt Yanchek <Yanchek99@users.noreply.github.com>
2026-07-07 21:12:46 -0700

    Have cf_wod:fetch parse the WOD, not just fetch it
    Now that CfWod::WorkoutParser exists, wire it into the dev rake task so
    it's actually useful for spot-checking real dates end to end.

--- test/tasks/cf_wod_rake_test.rb
+++ test/tasks/cf_wod_rake_test.rb
@@ -7,13 +7,14 @@ class CfWodRakeTest < ActiveSupport::TestCase
     Rake::Task['cf_wod:fetch'].reenable
   end
 
-  test 'fetches and prints a WOD for a given date' do
+  test 'fetches, parses, and prints a WOD for a given date' do
     stub_request(:get, %r{\Ahttps://www\.crossfit\.com/workout/2018/01/10})
       .to_return(status: 200, body: Rails.root.join('test/fixtures/cf_wod/legacy_with_scaling.html').read)
 
     output = capture_io { Rake::Task['cf_wod:fetch'].invoke('2018-01-10') }.join
 
     assert_includes output, 'slug="180110"'
+    assert_includes output, 'name: "CF-180110"'
   end
 
   test 'aborts with a usage message when no date is given' do
@@ -26,6 +27,17 @@ class CfWodRakeTest < ActiveSupport::TestCase
     stub_request(:get, %r{\Ahttps://www\.crossfit\.com/workout/2030/01/01}).to_return(status: 404)
 
     error = assert_raises(SystemExit) { Rake::Task['cf_wod:fetch'].invoke('2030-01-01') }
+
+    assert_equal 1, error.status
+  end
+
+  test 'aborts with the parse error message on an unparseable WOD' do
+    stub_request(:get, %r{\Ahttps://www\.crossfit\.com/workout/2026/06/20})
+      .to_return(status: 301, headers: { 'Location' => '/260620' })
+    stub_request(:get, %r{\Ahttps://www\.crossfit\.com/260620})
+      .to_return(status: 200, body: Rails.root.join('test/fixtures/cf_wod/modern_multi_part.html').read)
+
+    error = assert_raises(SystemExit) { Rake::Task['cf_wod:fetch'].invoke('2026-06-20') }
 
     assert_equal 1, error.status
   end

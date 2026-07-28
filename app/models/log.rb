3dd521d56c8a69fa681eaa232c7b0bb61667601c
Matt Yanchek <Yanchek99@users.noreply.github.com>
2026-07-28 07:01:19 -0700

    Revert "Add Log.orphaned scope with RailsAdmin visibility (#1824)" (#1827)

--- app/models/log.rb
+++ app/models/log.rb
@@ -8,8 +8,6 @@ class Log < ApplicationRecord
 
   accepts_nested_attributes_for :movement_logs, allow_destroy: true
 
-  scope :orphaned, -> { where.missing(:workout) }
-
   enum :score_type, Metric.measurements, prefix: :score
 
   validates :score_type, presence: true

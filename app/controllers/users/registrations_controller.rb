cdc7724952285bb2f291c5f6f3384ea44b2280b3
Matt Yanchek <Yanchek99@users.noreply.github.com>
2026-08-03 14:24:06 -0700

    Allow profile updates without current password

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    protected
+
+    def update_resource(resource, params)
+      if params[:password].present? || (params[:email].present? && params[:email].to_s != resource.email)
+        resource.update_with_password(params)
+      else
+        resource.update_without_password(params.except(:password, :password_confirmation, :current_password))
+      end
+    end
+  end
+end
---
+++ test/controllers/devise/registrations_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+module Devise
+  class RegistrationsControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
+
+    setup do
+      @user = users(:mathew)
+      @user.update!(password: 'password123', password_confirmation: 'password123')
+      sign_in @user
+    end
+
+    test 'profile-only updates do not require the current password' do
+      put user_registration_url, params: { user: { first_name: 'Matthew' } }
+
+      assert_redirected_to root_url
+      assert_equal 'Matthew', @user.reload.first_name
+    end
+
+    test 'email updates require the current password' do
+      put user_registration_url, params: { user: { email: 'new@example.com' } }
+
+      assert_response :unprocessable_entity
+      assert_equal 'mathew.fraser@wod-tracker.com', @user.reload.email
+    end
+
+    test 'password updates require the current password' do
+      put user_registration_url, params: {
+        user: { password: 'new-password123', password_confirmation: 'new-password123' }
+      }
+
+      assert_response :unprocessable_entity
+      assert @user.reload.valid_password?('password123')
+    end
+
+    test 'email and password updates accept the correct current password' do
+      put user_registration_url, params: {
+        user: {
+          email: 'new@example.com',
+          password: 'new-password123',
+          password_confirmation: 'new-password123',
+          current_password: 'password123'
+        }
+      }
+
+      assert_redirected_to root_url
+      assert_equal 'new@example.com', @user.reload.email
+      assert @user.valid_password?('new-password123')
+    end
+
+    test 'edit form does not universally require the current password' do
+      get edit_user_registration_url
+
+      assert_response :success
+      assert_select 'input[name="user[current_password]"]:not([required])'
+    end
+  end
+end
